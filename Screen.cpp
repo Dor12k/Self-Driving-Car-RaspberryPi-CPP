107c5492bab96b630dbc5cb6d9baca0d8c198410
Dor12k <107938584+Dor12k@users.noreply.github.com>
2025-06-04 16:26:53 +0300

    Add files via upload

--- Screen.cpp
+++ Screen.cpp
@@ -1,14 +1,14 @@
 
 # include "Screen.h"
 
-
-
 // Function print fps on the screen
-void Screen::design_frame(Mat &image, int fps) {
+void Screen::design_frame(Mat &image, int fps, float speed) {
 
-	string txt = to_string(fps);
+	string FPS = to_string(fps);
+	string spd = to_string(int(speed * 100)) + "cm/s";
 
-	putText(image, txt, Point(460, 335), FONT_HERSHEY_COMPLEX, 0.7, Scalar(255, 255, 255), 2);
+	putText(image, FPS, Point(460, 335), FONT_HERSHEY_COMPLEX, 0.7, Scalar(255, 255, 255), 2);
+	putText(image, spd, Point(5, 335), FONT_HERSHEY_COMPLEX, 0.7, Scalar(255, 255, 255), 2);
 }
 
 // Function display image on the screen
